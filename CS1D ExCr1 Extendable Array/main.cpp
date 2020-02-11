94f8e7266605e88a343a4836aaa2fdb8e3cf1e83
Nick Reardon <32754140+ThingOfNightmare@users.noreply.github.com>
2020-02-11 10:34:02 -0800

    Add project files.

---
+++ CS1D ExCr1 Extendable Array/main.cpp
@@ -0,0 +1,27 @@
+/*****************************************************************************
+ * AUTHOR 	  		: Nick Reardon
+ * Extra Credit #1	: Extendable Array
+ * CLASS			: CS1D
+ * SECTION			: MW - 2:30p
+ * DUE DATE			: 02 / 12 / 20
+ *****************************************************************************/
+#include "main.h"
+
+using std::cout; using std::endl;
+
+
+int main()
+{
+
+	/*
+	 * HEADER OUTPUT
+	 */
+	PrintHeader(cout, "Prompt.txt");
+
+	/********************************************************************/
+
+
+
+	system("pause");
+	return 0;
+}
---
+++ CS1D ExCr1 Extendable Array/main.h
@@ -0,0 +1,21 @@
+/*****************************************************************************
+ * AUTHOR 	  		: Nick Reardon
+ * Extra Credit #1	: Extendable Array
+ * CLASS			: CS1D
+ * SECTION			: MW - 2:30p
+ * DUE DATE			: 02 / 12 / 20
+ *****************************************************************************/
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+//Standard includes
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include "PrintHeader.h"
+
+//Program Specific
+
+
+#endif // _HEADER_H_
+
